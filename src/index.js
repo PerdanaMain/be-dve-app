b56450c34a3b7b38909ebf34fcf24addc7ba1567
perdanamain <firman.fp123@gmail.com>
2025-07-19 13:53:10 +0700

    feat: initial commit

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import { port } from "./utils/env.js";
+
+const main = async () => {
+  try {
+    const app = express();
+
+    app.use(cors());
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
+
+    app.get("/", (_, res) => {
+      res.status(200).json({
+        message: "DVE API Ready!",
+        data: null,
+      });
+    });
+
+    app.listen(port, () => {
+      console.log(`Server is Running on port ${port}`);
+    });
+  } catch (error) {
+    console.log("Error: ", error);
+  }
+};
+
+main();
